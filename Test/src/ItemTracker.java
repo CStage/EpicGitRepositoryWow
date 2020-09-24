3cee30169e27075c6eeb2f46e6ccdf9fe73492f2
Christian <c.stage@live.dk>
2020-09-24 13:09:16 +0200

    add items field

--- Test/src/ItemTracker.java
+++ Test/src/ItemTracker.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 public class ItemTracker {
+    private ArrayList<String> items;
     public static void main(String[] args) throws Exception {
         System.out.println("Hej Johannes");
     }
