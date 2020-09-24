182ec0d465cd8dea9ab4575827701ac0ee80bdce
Ola Johannes Rostad Bertoft <olbe@itu.dk>
2020-09-24 13:12:21 +0200

    Update ItemTracker.java

--- Test/src/ItemTracker.java
+++ Test/src/ItemTracker.java
@@ -2,6 +2,18 @@ import java.util.ArrayList;
 
 public class ItemTracker {
     private ArrayList<String> items;
+
+    public ItemTracker() {
+        items = new ArrayList<>();
+    }
+
+    //1. Add method
+
+    public void addItems(String item) {
+        items.add(item);
+    }
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hej Johannes");
     }
