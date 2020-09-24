2dc595f2f2764eef3ab3d96f5970e72614bfa997
Christian <c.stage@live.dk>
2020-09-24 13:53:32 +0200

    Create Parser.java

---
+++ Test/Parser.java
@@ -0,0 +1,19 @@
+public class Parser {
+    private HashMap<Integer, String> commandMap;
+
+    public Parser() {
+        commandMap = new HashMap<Integer, String>();
+        commandMap.put(1, "Add item");
+        commandMap.put(2, "Remove item");
+        commandMap.put(3, "Get item");
+        commandMap.put(4, "See items");
+        commanMap.put(5, "Quit");
+
+    }
+    
+    public void acceptInput() {
+        ;
+    }
+    
+}
+
