d247eaf8f5f6360b56cad7d75219d3fc33524bc7
Ola Johannes Rostad Bertoft <olbe@itu.dk>
2020-10-08 12:52:39 +0200

    Create Masks.java

---
+++ Test/src/main/java/Test/Masks.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Masks {
+    
+    
+    public Masks(String name, int price, int age) {
+        super(name, price, age);
+        this.depreciationRate = 0.3;
+    }
+
+    
+}
